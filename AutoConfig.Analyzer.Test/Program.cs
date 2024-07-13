5d2e47634f9a5a45180bca44dcb16206849ac939
xfe18827675401 <xfe18827675401@126.com>
2024-07-14 04:55:48 +0800

    add-为每个配置文件类增加静态ProfilePath属性，可以使用引用路径，优先级大于ProfilePath特性

---
+++ AutoConfig.Analyzer.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoConfig.Analyzer.Test;
+
+public class Program
+{
+    [SMTest]
+    public static void MainTest()
+    {
+        SystemProfile.TestNum = 3;
+        Console.WriteLine(SystemProfile.TestNum);
+        Console.WriteLine(SystemProfile.ProfilePath);
+    }
+}
